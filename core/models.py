65e151c65e59393100c860ff64335dacf32a4341
Ahmad Dzulfikar Ubaidillah <dzulfikarubaid@gmail.com>
2023-07-14 07:40:51 +0700

    initial commit

--- core/models.py
+++ core/models.py
@@ -5,11 +5,11 @@ class Post(models.Model):
     body = models.TextField()
     harga_asli = models.IntegerField()
     harga_diskon = models.IntegerField()
-    image1 = models.ImageField(upload_to='tmp/', blank=True, default='')
-    image2 = models.ImageField(upload_to='tmp/', blank=True, default='')
-    image3 = models.ImageField(upload_to='tmp/', blank=True, default='')
-    image4 = models.ImageField(upload_to='tmp/', blank=True, default='')
-    image5 = models.ImageField(upload_to='tmp/', blank=True, default='')
+    image1 = models.TextField(blank=True, default='')
+    image2 = models.TextField(blank=True, default='')
+    image3 = models.TextField(blank=True, default='')
+    image4 = models.TextField(blank=True, default='')
+    image5 = models.TextField(blank=True, default='')
 
     def __str__(self):
         return self.title
